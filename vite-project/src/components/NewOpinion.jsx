119499888a7403afbe9d5b00a312881c02e8768b
Sanish Suwal <sanishsuwal3@gmail.com>
2024-12-16 10:57:21 -0500

    very tpucghj thing maaan

---
+++ vite-project/src/components/NewOpinion.jsx
@@ -0,0 +1,67 @@
+import {useActionState, use} from 'react';
+import { OpinionsContext } from '../store/opinions-context';
+import Submit from './Submit';
+
+export function NewOpinion() {
+  const {addOpinion} = use(OpinionsContext)
+  async function shareOpinionAction(prevState, formData){
+    
+    const title = formData.get("title");
+    const body = formData.get("body");
+    const userName = formData.get("userName");
+
+    let errors = [];
+
+    if(title.trim().length < 5){
+      errors.push("Title is too short");
+    }
+
+    if (body.trim().length < 10 || body.trim().length > 3000) {
+      errors.push("Opinion should be between 10 and 300 characters");
+    }
+
+    if(!userName.trim()) {
+      errors.push("Please enter your name");
+    }
+
+    if (errors.length > 0) {
+      return { errors, enteredValues: { userName, title, body } };
+    }
+
+    await addOpinion({title, body, userName})
+    return {errors:null}
+  }
+
+  const [formState, formAction] = useActionState(shareOpinionAction, {errors:null});
+  
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={formAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input type="text" id="userName" name="userName" defaultValue={formState.enteredValues?.userName}/>
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" defaultValue={formState.enteredValues?.title}/>
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea id="body" name="body" rows={5} defaultValue={formState.enteredValues?.body}></textarea>
+        </p>
+        {formState.errors && (<ul className='errors'>
+          {formState.errors.map((error) => (
+            <li key={error}>
+              {error}
+            </li>
+          ))}
+        </ul>)}
+          <Submit />
+      </form>
+    </div>
+  );
+}
